98ce7b65be2e687ca80b3d0255314c7125654277
Spartacus <k_morter@outlook.com>
2020-05-13 21:48:18 -0600

    added all the password prompts and button listener

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,6 @@
-// Assignment code here 
-
-var length = prompt("Choose a password length between 8 and 128");
-console.log(length);
-
-var specialCharacters = confirm("Do you want to include speial characters?")
-console.log(specialCharacters);
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate"); 
-
-// Write password to the #password input
+//When you click the "generate password" button, prompts will begin
+var generateBtn = document.querySelector("#generate");
+ 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -18,5 +9,23 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//After you click the "generate password" button, the generatePassword() will start with the list of questions.
+function generatePassword (){
+  var length = prompt("Please enter the number of characters you want for you new password. It must be more than 8 but less than 128.");
+  console.log(length);
+
+  var number = confirm("Do you want numbers in your password?");
+  console.log(number);
+
+  var lowerCases = confirm("Do you want lowercase characters?");
+  console.log(lowerCases);
+
+  var upperCases = confirm("Do you want uppercase characters?");
+  console.log(upperCases);
+
+  var special = confirm("Do you want special characters?");
+  console.log(special);
+}
+
