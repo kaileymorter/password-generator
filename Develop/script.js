2c9b1d5572ca11f64faf16fd79d360ae2a0ee14b
Spartacus <k_morter@outlook.com>
2020-05-13 20:58:05 -0600

    added first prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,13 @@
-// Assignment code here
+// Assignment code here 
 
+var length = prompt("Choose a password length between 8 and 128");
+console.log(length);
+
+var specialCharacters = confirm("Do you want to include speial characters?")
+console.log(specialCharacters);
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
 
 // Write password to the #password input
 function writePassword() {
